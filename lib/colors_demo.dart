2f2d68c72af42cacd3943353641ee2cc4005f279
Arthur <feixiangdebaiyun@yeah.net>
2019-01-01 15:10:24 +0800

    删除不必要的代码
    删除不必要的代码，方便说明

--- lib/colors_demo.dart
+++ lib/colors_demo.dart
@@ -43,33 +43,30 @@ class _Palette {
   //accent color
   //重点色;强调色;重点色彩
   final MaterialAccentColor accent;
-  //阀值
-  final int threshold; // titles for indices > threshold are white, otherwise black
 
-  bool get isValid => name != null && primary != null && threshold != null;
 }
 
 //数据源
 final List<_Palette> _allPalettes = <_Palette>[
-  _Palette(name: 'RED', primary: Colors.red, accent: Colors.redAccent, threshold: 300),
-  _Palette(name: 'PINK', primary: Colors.pink, accent: Colors.pinkAccent, threshold: 200),
-  _Palette(name: 'PURPLE', primary: Colors.purple, accent: Colors.purpleAccent, threshold: 200),
-  _Palette(name: 'DEEP PURPLE', primary: Colors.deepPurple, accent: Colors.deepPurpleAccent, threshold: 200),
-  _Palette(name: 'INDIGO', primary: Colors.indigo, accent: Colors.indigoAccent, threshold: 200),
-  _Palette(name: 'BLUE', primary: Colors.blue, accent: Colors.blueAccent, threshold: 400),
-  _Palette(name: 'LIGHT BLUE', primary: Colors.lightBlue, accent: Colors.lightBlueAccent, threshold: 500),
-  _Palette(name: 'CYAN', primary: Colors.cyan, accent: Colors.cyanAccent, threshold: 600),
-  _Palette(name: 'TEAL', primary: Colors.teal, accent: Colors.tealAccent, threshold: 400),
-  _Palette(name: 'GREEN', primary: Colors.green, accent: Colors.greenAccent, threshold: 500),
-  _Palette(name: 'LIGHT GREEN', primary: Colors.lightGreen, accent: Colors.lightGreenAccent, threshold: 600),
-  _Palette(name: 'LIME', primary: Colors.lime, accent: Colors.limeAccent, threshold: 800),
+  _Palette(name: 'RED', primary: Colors.red, accent: Colors.redAccent),
+  _Palette(name: 'PINK', primary: Colors.pink, accent: Colors.pinkAccent),
+  _Palette(name: 'PURPLE', primary: Colors.purple, accent: Colors.purpleAccent),
+  _Palette(name: 'DEEP PURPLE', primary: Colors.deepPurple, accent: Colors.deepPurpleAccent),
+  _Palette(name: 'INDIGO', primary: Colors.indigo, accent: Colors.indigoAccent),
+  _Palette(name: 'BLUE', primary: Colors.blue, accent: Colors.blueAccent),
+  _Palette(name: 'LIGHT BLUE', primary: Colors.lightBlue, accent: Colors.lightBlueAccent),
+  _Palette(name: 'CYAN', primary: Colors.cyan, accent: Colors.cyanAccent),
+  _Palette(name: 'TEAL', primary: Colors.teal, accent: Colors.tealAccent),
+  _Palette(name: 'GREEN', primary: Colors.green, accent: Colors.greenAccent),
+  _Palette(name: 'LIGHT GREEN', primary: Colors.lightGreen, accent: Colors.lightGreenAccent),
+  _Palette(name: 'LIME', primary: Colors.lime, accent: Colors.limeAccent),
   _Palette(name: 'YELLOW', primary: Colors.yellow, accent: Colors.yellowAccent),
   _Palette(name: 'AMBER', primary: Colors.amber, accent: Colors.amberAccent),
-  _Palette(name: 'ORANGE', primary: Colors.orange, accent: Colors.orangeAccent, threshold: 700),
-  _Palette(name: 'DEEP ORANGE', primary: Colors.deepOrange, accent: Colors.deepOrangeAccent, threshold: 400),
-  _Palette(name: 'BROWN', primary: Colors.brown, threshold: 200),
-  _Palette(name: 'GREY', primary: Colors.grey, threshold: 500),
-  _Palette(name: 'BLUE GREY', primary: Colors.blueGrey, threshold: 500),
+  _Palette(name: 'ORANGE', primary: Colors.orange, accent: Colors.orangeAccent),
+  _Palette(name: 'DEEP ORANGE', primary: Colors.deepOrange, accent: Colors.deepOrangeAccent),
+  _Palette(name: 'BROWN', primary: Colors.brown),
+  _Palette(name: 'GREY', primary: Colors.grey),
+  _Palette(name: 'BLUE GREY', primary: Colors.blueGrey),
 ];
 
 
@@ -107,7 +104,7 @@ class _ColorItem extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text('$prefix$index'),
+              Text('$prefix $index'),
               Text(colorString()),
             ],
           ),
@@ -126,31 +123,9 @@ class _PaletteTabView extends StatelessWidget {
        super(key: key);
 
   final _Palette colors;
-//  static const MaterialColor red = MaterialColor(
-//    _redPrimaryValue,
-//    <int, Color>{
-//      50: Color(0xFFFFEBEE),
-//      100: Color(0xFFFFCDD2),
-//      200: Color(0xFFEF9A9A),
-//      300: Color(0xFFE57373),
-//      400: Color(0xFFEF5350),
-//      500: Color(_redPrimaryValue),
-//      600: Color(0xFFE53935),
-//      700: Color(0xFFD32F2F),
-//      800: Color(0xFFC62828),
-//      900: Color(0xFFB71C1C),
-//    },
-//  );
+
   static const List<int> primaryKeys = <int>[50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
-//  static const MaterialAccentColor redAccent = MaterialAccentColor(
-//    _redAccentValue,
-//    <int, Color>{
-//      100: Color(0xFFFF8A80),
-//      200: Color(_redAccentValue),
-//      400: Color(0xFFFF1744),
-//      700: Color(0xFFD50000),
-//    },
-//  );
+
   static const List<int> accentKeys = <int>[100, 200, 400, 700];
 
   @override
@@ -161,7 +136,7 @@ class _PaletteTabView extends StatelessWidget {
     final List<Widget> colorItems = primaryKeys.map<Widget>((int value) {
       return DefaultTextStyle(
         style: value > colors.threshold ? whiteTextStyle : blackTextStyle,
-        child: _ColorItem(index: value, color: colors.primary[value]),
+        child: _ColorItem(index: value, color: colors.primary[value],prefix: 'Primary Color'),
       );
     }).toList();
 
@@ -169,7 +144,7 @@ class _PaletteTabView extends StatelessWidget {
       colorItems.addAll(accentKeys.map<Widget>((int index) {
         return DefaultTextStyle(
           style: index > colors.threshold ? whiteTextStyle : blackTextStyle,
-          child: _ColorItem(index: index, color: colors.accent[index], prefix: 'A'),
+          child: _ColorItem(index: index, color: colors.accent[index], prefix: 'AccentColor'),
         );
       }).toList());
     }
